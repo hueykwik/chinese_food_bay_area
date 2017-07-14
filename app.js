37a7075be25d4a6c4222906414ccabe74fc6892e
Huey Kwik <huey.kwik@gmail.com>
2017-07-14 16:47:29 -0700

    Remove unnecessary function.

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-function createLocations() {
-  var locations = [
-    {title: 'Gou Bu Li', location: {lat: 37.961543, lng: -122.325498}},
-    {title: 'Hometown Noodle', location: {lat: 37.261173, lng: -121.932017}},
-    {title: 'T4 Livermore', location: {lat: 37.680343, lng: -121.747897}},
-    {title: 'T4 Livermore', location: {lat: 37.680343, lng:-121.747897}},
-    {title: 'Sichuan Table', location: {lat: 37.775155, lng: -122.50606}},
-    {title: 'Tashi delek', location: {lat: 37.919973, lng: -122.314227}}
-  ];
-
-  return locations;
-}
+var locations = [
+  {title: 'Gou Bu Li', location: {lat: 37.961543, lng: -122.325498}},
+  {title: 'Hometown Noodle', location: {lat: 37.261173, lng: -121.932017}},
+  {title: 'T4 Livermore', location: {lat: 37.680343, lng: -121.747897}},
+  {title: 'T4 Livermore', location: {lat: 37.680343, lng:-121.747897}},
+  {title: 'Sichuan Table', location: {lat: 37.775155, lng: -122.50606}},
+  {title: 'Tashi delek', location: {lat: 37.919973, lng: -122.314227}}
+];
 
 function initMap() {
   var sfba = {lat: 37.8272, lng: -122.2913};
@@ -18,8 +14,7 @@ function initMap() {
     center: sfba
   });
 
-  var locations = createLocations();
-  addMarkersToMap(map, locations)
+  addMarkersToMap(map, locations);
 }
 
 function addMarkersToMap(map, locations) {
