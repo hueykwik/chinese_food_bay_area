09905eeeecf7f03dff9720851cffa0d20185558d
Huey Kwik <huey.kwik@gmail.com>
2017-07-17 16:02:38 -0700

    Create an observable array of locations and bind it to a list view.

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ var locations = [
   {title: 'Gou Bu Li', location: {lat: 37.961543, lng: -122.325498}},
   {title: 'Hometown Noodle', location: {lat: 37.261173, lng: -121.932017}},
   {title: 'T4 Livermore', location: {lat: 37.680343, lng: -121.747897}},
-  {title: 'T4 Livermore', location: {lat: 37.680343, lng:-121.747897}},
   {title: 'Sichuan Table', location: {lat: 37.775155, lng: -122.50606}},
   {title: 'Tashi delek', location: {lat: 37.919973, lng: -122.314227}}
 ];
+
+var ViewModel = function() {
+  var self = this;
+
+  self.locationList = ko.observableArray(locations);
+}
+
+ko.applyBindings(new ViewModel());
 
 function initMap() {
   var sfba = {lat: 37.8272, lng: -122.2913};
@@ -36,3 +43,5 @@ function addMarkersToMap(map, locations) {
 
   map.fitBounds(bounds);
 }
+
+
