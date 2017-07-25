9ddaecd1eff9335d1e9f86ff86b55d8383d889e0
Huey Kwik <huey.kwik@gmail.com>
2017-07-24 19:46:22 -0700

    Factor Yelp API code into its own module.

---
+++ yelp.py
@@ -0,0 +1,146 @@
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+from urllib.error import HTTPError
+from urllib.parse import quote
+from urllib.parse import urlencode
+
+import credentials
+
+# Fill in your Yelp OAuth credentials here.
+CLIENT_ID = credentials.CLIENT_ID
+CLIENT_SECRET = credentials.CLIENT_SECRET
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+TOKEN_PATH = '/oauth2/token'
+GRANT_TYPE = 'client_credentials'
+
+
+# Defaults for our simple example.
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 3
+
+
+def obtain_bearer_token(host, path):
+    """Given a bearer token, send a GET request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        url_params (dict): An optional set of query parameters in the request.
+
+    Returns:
+        str: OAuth bearer token, obtained using client_id and client_secret.
+
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    assert CLIENT_ID, "Please supply your client_id."
+    assert CLIENT_SECRET, "Please supply your client_secret."
+    data = urlencode({
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': GRANT_TYPE,
+    })
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.request('POST', url, data=data, headers=headers)
+    bearer_token = response.json()['access_token']
+    return bearer_token
+
+def request(host, path, bearer_token, url_params=None):
+    """Given a bearer token, send a GET request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        bearer_token (str): OAuth bearer token, obtained using client_id and client_secret.
+        url_params (dict): An optional set of query parameters in the request.
+
+    Returns:
+        dict: The JSON response from the request.
+
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % bearer_token,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+def search(bearer_token, term, location):
+    """Query the Search API by a search term and location.
+
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, bearer_token, url_params=url_params)
+
+
+def get_business(bearer_token, business_id):
+    """Query the Business API by a business ID.
+
+    Args:
+        business_id (str): The ID of the business to query.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, bearer_token)
+
+
+def query_api(term, location):
+    """Queries the API by the input values from the user.
+
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    bearer_token = obtain_bearer_token(API_HOST, TOKEN_PATH)
+
+    response = search(bearer_token, term, location)
+
+    businesses = response.get('businesses')
+
+    if not businesses:
+        print(u'No businesses for {0} in {1} found.'.format(term, location))
+        return
+
+    business_id = businesses[0]['id']
+
+    print(u'{0} businesses found, querying business info ' \
+        'for the top result "{1}" ...'.format(
+            len(businesses), business_id))
+    response = get_business(bearer_token, business_id)
+
+    print(u'Result for business "{0}" found:'.format(business_id))
+    pprint.pprint(response, indent=2)
