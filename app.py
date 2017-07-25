8a97ff5825620d3244959df89993f13f37a1879c
Huey Kwik <huey.kwik@gmail.com>
2017-07-25 13:15:41 -0700

    Print out the name of each restaurant from the KML file in the console.

--- app.py
+++ app.py
@@ -14,15 +14,42 @@ locations = [
     {'name': 'Tashi delek2', 'lat': 37.919973, 'lng': -122.314227}
 ];
 
+def convertPlacemarksToLocations(placemarks):
+    locations = []
+
+    print(placemarks[0].name)
+
+    for placemark in placemarks[0].features():
+        print(placemark.name)
+
+    # for placemark in placemarks:
+    #     location = {}
+    #     location['name'] = placemark.name
+
+    #     print(placemark.name)
+    #     print(placemark.features())
+    #     # coordinates = placemark.point.coordinates.split(',')
+
+    #     # location['lng'] = coordinates[0]
+    #     # location['lat'] = coordinates[1]
+
+    #     locations.append(location)
+
+    return locations
+
 @app.route("/")
 def showMap():
     with open('regional-chinese-sf-bay-area.kml') as f:
         doc = f.read()
         k = kml.KML()
         k.from_string(doc)
-        print(len(k._features))
-        print(doc)
-    return render_template("index.html", locations=locations)
+
+        features = list(k.features())
+        folder = list(features[0].features())
+
+        locations = convertPlacemarksToLocations(folder)
+
+        return render_template("index.html", locations=locations)
 
 @app.route("/business")
 def getBusinessInfo():
